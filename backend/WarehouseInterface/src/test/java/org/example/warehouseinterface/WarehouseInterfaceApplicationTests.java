985d3a96c760dda07b1d19a85d1006246d613356
Henry Fielding <hfielding@student.unimelb.edu.au>
2024-08-22 15:55:55 +1000

    Initialise springboot project files

---
+++ backend/WarehouseInterface/src/test/java/org/example/warehouseinterface/WarehouseInterfaceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.warehouseinterface;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WarehouseInterfaceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
