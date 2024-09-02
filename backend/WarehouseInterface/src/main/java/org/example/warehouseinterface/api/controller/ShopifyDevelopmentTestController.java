45f5cd175f635009126162472ee8d3567de30e95
Henry Fielding <hfielding@student.unimelb.edu.au>
2024-09-02 14:41:13 +1000

    Integrate development store with backend service

---
+++ backend/WarehouseInterface/src/main/java/org/example/warehouseinterface/api/controller/ShopifyDevelopmentTestController.java
@@ -0,0 +1,36 @@
+package org.example.warehouseinterface.api.controller;
+
+import org.example.warehouseinterface.api.model.BaxterBox;
+import org.example.warehouseinterface.service.ShopifyDevelopmentTestService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.net.http.HttpResponse;
+
+@RestController
+public class ShopifyDevelopmentTestController {
+
+    private ShopifyDevelopmentTestService service;
+
+    @Autowired
+    public ShopifyDevelopmentTestController(ShopifyDevelopmentTestService service) {
+        this.service = service;
+    }
+
+    @GetMapping("/ShopifyDevTest")
+    public String getAllProducts() {
+        try {
+            return service.getAllProducts().toString();
+        } catch (Exception e) {
+            // Log the exception (optional)
+            e.printStackTrace();
+            // Return an appropriate error response
+            return null;
+        }
+    }
+
+}
