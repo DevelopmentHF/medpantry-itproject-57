6ee9d5acbf847a3a10b4e8b7cecbffd23dbdc4df
Henry Fielding <hfielding@student.unimelb.edu.au>
2024-09-05 11:48:47 +1000

    Conditional rendering of full/not full baxter box

--- frontend/medpantry/components/BaxterBox.tsx
+++ frontend/medpantry/components/BaxterBox.tsx
@@ -13,17 +13,19 @@ interface BaxterBoxProps {
   isFull: boolean;
 }
 
-export default function BaxterBox({ warehouseId, id, sku, units, isFull, bgColor = 'bg-card' }: BaxterBoxProps) {
+export default function BaxterBox({ warehouseId, id, sku, units, isFull, bgColor = 'bg-secondary' }: BaxterBoxProps) {
 
   return (  
     <div className={`${bgColor} border-solid border-border rounded-md p-4 flex flex-col gap-2`}>
-        <div>
-            <h2 className='text-card-foreground'>id: {id}</h2>
-            <h2 className='text-card-foreground'>SKU: {sku}</h2>
-            <h2 className='text-card-foreground'>warehouseId: {warehouseId}</h2>
-            <h2 className='text-card-foreground'>units: {units}</h2>
-            <h2 className='text-card-foreground'>is full: {isFull ? 'true' : 'false'}</h2>
-        </div>
+      <h1 className='sm:text-lg md:text-xl lg:text-2xl font-bold'>Baxter Box #{id}</h1>
+      <h2 className={`sm:text-sm md:text-md lg:text-lg font-bold ${isFull ? 'text-red-500' : 'text-green-500'}`}>
+          {isFull ? 'Full' : 'Not full'}
+      </h2>
+      <h2 className='sm:text-sm md:text-md lg:text-lg'>{units} units</h2>
+      <div>
+          <h2 className='sm:text-sm md:text-md lg:text-lg'>Contains  SKU#{sku}</h2>
+          <h2 className='sm:text-sm md:text-md lg:text-lg'>Located in Warehouse {warehouseId}</h2>
+      </div>
     </div>
   );
 }
