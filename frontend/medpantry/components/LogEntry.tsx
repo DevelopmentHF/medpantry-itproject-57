f9df64f2be81fa376632e3be32417c4f1c0b751e
Henry Fielding <hfielding@student.unimelb.edu.au>
2024-09-10 17:55:53 +1000

    Style adjustments for log entry

--- frontend/medpantry/components/LogEntry.tsx
+++ frontend/medpantry/components/LogEntry.tsx
@@ -1,4 +1,5 @@
 import { Button } from "./ui/button";
+import { Separator } from "./ui/separator";
 
 interface LogEntryProps {
     id: number;
@@ -13,16 +14,32 @@ interface LogEntryProps {
 export default function LogEntry({ id, box, sku, proposedQuantityToAdd, pending, accepted } : LogEntryProps) {
     return (
         <>
-        <div className={`bg-secondary border-solid border-border rounded-md p-4 flex flex-col gap-2`}>
-            <p>ID: {id}</p>
-            <p>Box: {box}</p>
-            <p>SKU: {sku}</p>
-            <p>Proposed Quantity: {proposedQuantityToAdd}</p>
-            <div className="flex gap-4">
-                <Button className="bg-green-500">Accept</Button>
-                <Button className="bg-red-500">Reject</Button>
+            <div className={`bg-secondary border-solid border-border rounded-md p-4 flex flex-col gap-2`}>
+
+                <h1 className="font-bold text-xl">Stock Update</h1>
+
+                <div className="flex justify-between">
+                    <p className="">Baxter Box</p>
+                    <p className="text-gray-400">#{box}</p>
+                </div>
+                
+                <div className="flex justify-between">
+                    <p className="">Product</p>
+                    <p className="text-gray-400">#{sku}</p>
+                </div>
+                
+                <div className="flex justify-between">
+                    <p className="">Quantity</p>
+                    <p className="text-gray-400">+{proposedQuantityToAdd}</p>
+                </div>
+
+                <Separator />
+
+                <div className="flex gap-4">
+                    <Button className="bg-green-500">Accept</Button>
+                    <Button className="bg-red-500">Reject</Button>
+                </div>
             </div>
-        </div>
         </>
     )
 }
