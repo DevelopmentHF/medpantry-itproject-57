bece967d8179f2755fd37a616fac0f6be3a63a40
Henry Fielding <hfielding@student.unimelb.edu.au>
2024-09-10 16:25:22 +1000

    Basic log entry styling

--- frontend/medpantry/components/LogEntry.tsx
+++ frontend/medpantry/components/LogEntry.tsx
@@ -1,3 +1,5 @@
+import { Button } from "./ui/button";
+
 interface LogEntryProps {
     id: number;
     box: number;
@@ -11,13 +13,15 @@ interface LogEntryProps {
 export default function LogEntry({ id, box, sku, proposedQuantityToAdd, pending, accepted } : LogEntryProps) {
     return (
         <>
-        <div className="log-entry">
+        <div className={`bg-secondary border-solid border-border rounded-md p-4 flex flex-col gap-2`}>
             <p>ID: {id}</p>
             <p>Box: {box}</p>
             <p>SKU: {sku}</p>
             <p>Proposed Quantity: {proposedQuantityToAdd}</p>
-            <p>Pending: {pending ? "Yes" : "No"}</p>
-            <p>Accepted: {accepted ? "Yes" : "No"}</p>
+            <div className="flex gap-4">
+                <Button className="bg-green-500">Accept</Button>
+                <Button className="bg-red-500">Reject</Button>
+            </div>
         </div>
         </>
     )
