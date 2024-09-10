0ae838f4ae697d48c96b56b8b884002a88409b83
Henry Fielding <hfielding@student.unimelb.edu.au>
2024-09-10 18:00:49 +1000

    Rearrange nav bar

--- frontend/medpantry/app/protected/add-to-stock/page.tsx
+++ frontend/medpantry/app/protected/add-to-stock/page.tsx
@@ -10,11 +10,14 @@ export default function AddToStockPage() {
 
     return (
         <div className="flex-1 w-full flex flex-col gap-12 items-center p-6">
-            <nav className="flex gap-4 border-b border-b-foreground/10 h-16 w-full items-center">
+            <nav className="flex gap-4 border-b border-b-foreground/10 h-16 w-full items-center justify-between">
+                <Button>
+                    <a href="../protected">
+                        Home
+                    </a>
+                </Button>
+               
                 <AuthButton />
-                <a href="../protected">
-                    Go back
-                </a>
             </nav>
             <h1 className="font-bold text-4xl">Add to Stock</h1>
             <AddStock></AddStock>
--- frontend/medpantry/app/protected/manager-log/page.tsx
+++ frontend/medpantry/app/protected/manager-log/page.tsx
@@ -11,11 +11,15 @@ export default function ManagerLogPage() {
 
     return (
         <div className="flex-1 w-full flex flex-col gap-12 items-center p-6">
-            <nav className="flex gap-4 border-b border-b-foreground/10 h-16 w-full items-center">
+            <nav className="flex gap-4 border-b border-b-foreground/10 h-16 w-full items-center justify-between">
+                
+                <Button>
+                    <a href="../protected">
+                        Home
+                    </a>
+                </Button>
+               
                 <AuthButton />
-                <a href="../protected">
-                    Go back
-                </a>
             </nav>
             <h1 className="font-bold text-4xl">Manager Log</h1>
             <ManagerLog></ManagerLog>
