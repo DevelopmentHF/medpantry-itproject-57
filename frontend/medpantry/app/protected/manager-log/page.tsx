8446327a3ea738ad5a3ce075727a6a6124d71b0b
stakayanagi <stakayanagi@student.unimelb.edu.au>
2024-09-21 12:02:14 +1000

    handle errors and add comments on manager-log\page

--- frontend/medpantry/app/protected/manager-log/page.tsx
+++ frontend/medpantry/app/protected/manager-log/page.tsx
@@ -9,11 +9,26 @@ import ManagerLog from '@/components/ManagerLog';
 import { createClient } from "@/utils/supabase/server";
 
 export default async function ManagerLogPage() {
-
+    let isManagerAccount: boolean = false;
     let logEntries: any[] = [];
 
+    //check if the user is on a manager account
     try {
-        // NEED A .env see discord
+        const { data: { user } } = await createClient().auth.getUser();
+        console.log("User:", user?.id);  // Check if user data is correct
+        // manager account ID is HARDCODED
+        if (user?.id === "3915c954-4bfc-439f-8884-320b3d48e083") {
+            isManagerAccount = true;
+        }
+    } catch (error) {
+        console.error('Error in checking ID:', error);
+        return <p>Error fetching user data.</p>;
+    }
+
+    console.log("isManagerAccount: ", isManagerAccount);
+
+    //Obtain all pending log entries
+    try {
         const res = await fetch(`${process.env.NEXT_PUBLIC_BACKEND_LINK}/logEntries`);
         if (!res.ok) throw new Error('Network response was not ok');
         logEntries = await res.json();
@@ -21,37 +36,21 @@ export default async function ManagerLogPage() {
         console.log(logEntries);
     } catch (error) {
         console.error(error);
-        return null;
+        return <p>Error fetching log entries.</p>;
     }
-
-    //check if the user is on the manager account
-    let isManagerAccount: boolean = false;
-    try {
-        const { data: { user } } = await createClient().auth.getUser();
-        console.log("User:", user.id);  // Check if user data is correct
-        // manager account ID is HARDCODED
-        if (user?.id === "3915c954-4bfc-439f-8884-320b3d48e083") {
-            isManagerAccount = true;
-        }
-    } catch (error) {
-        console.error('Error in checking ID:', error);
-    }
-    console.log("isManagerAccount: ", isManagerAccount);
 
     return (
         <div className="flex-1 w-full flex flex-col gap-12 items-center p-6">
             <nav className="flex gap-4 border-b border-b-foreground/10 h-16 w-full items-center justify-between">
-
                 <Button>
                     <a href="../protected">
                         Home
                     </a>
                 </Button>
-
                 <AuthButton />
             </nav>
             <h1 className="font-bold text-4xl">Manager Log</h1>
-            <ManagerLog logEntries={logEntries} isManagerAccount={isManagerAccount}></ManagerLog>
+            <ManagerLog logEntries={logEntries} isManagerAccount={isManagerAccount} />
         </div>
     );
 }
