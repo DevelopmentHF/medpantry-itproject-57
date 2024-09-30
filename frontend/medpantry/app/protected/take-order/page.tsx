feb8d829220d46da399a3a1f83b8d15de4ff29f1
stakayanagi <stakayanagi@student.unimelb.edu.au>
2024-09-30 16:02:48 +1000

    created a new page to complete an order after taking it

---
+++ frontend/medpantry/app/protected/take-order/page.tsx
@@ -0,0 +1,12 @@
+import Order from "@/components/Order";
+
+interface TakeOrderProps {
+    order: Order
+}
+
+export default async function TakeOrder({ order }: TakeOrderProps) {
+
+    return (
+        <div></div>
+    );
+}
