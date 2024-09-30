0759a6063003f9ce3b91813deb4d381509132e0b
stakayanagi <stakayanagi@student.unimelb.edu.au>
2024-10-01 01:08:01 +1000

    navigation bar is added, but the home button just returns to dashboard

--- frontend/medpantry/app/protected/take-order/page.tsx
+++ frontend/medpantry/app/protected/take-order/page.tsx
@@ -1,4 +1,5 @@
 import Order from "@/components/Order";
+import AuthButton from "@/components/AuthButton";
 import { Button } from "@/components/ui/button";
 
 interface Data {
@@ -20,7 +21,17 @@ export default async function TakeOrder({ searchParams }: { searchParams: { orde
     console.log(orderNumber, datas, boxes);
 
     return (
-        <div>
+        <div className="flex-1 w-full flex flex-col gap-12 items-center p-6">
+            <nav className="flex gap-4 border-b border-b-foreground/10 h-16 w-full items-center justify-between">
+
+                <Button>
+                    <a href="../protected">
+                        Home
+                    </a>
+                </Button>
+
+                <AuthButton />
+            </nav>
             <Order
                 orderNumber={orderNumber}
                 datas={datas}
