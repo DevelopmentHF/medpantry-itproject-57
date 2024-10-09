5f9c2a14df3e0a35f539861fe0ce01b8bace1f9b
stakayanagi <stakayanagi@student.unimelb.edu.au>
2024-10-10 10:13:13 +1100

    temporary

--- frontend/medpantry/components/DoneOrderButton.tsx
+++ frontend/medpantry/components/DoneOrderButton.tsx
@@ -4,42 +4,40 @@ import { Button } from '@/components/ui/button';
 import { useRouter } from 'next/navigation';
 
 type DoneOrderButtonProps = {
-    orderNumber: string
+  orderNumber: string
 };
 
 export default function DoneOrderButton({ orderNumber }: DoneOrderButtonProps) {
-    const router = useRouter();
+  const router = useRouter();
 
-    const handleClick = async () => {
-        try {
-            const value: string = encodeURIComponent(orderNumber);
-            console.log(value);
+  const handleClick = async () => {
+    try {
+      const value: string = encodeURIComponent(orderNumber);
+      console.log(value);
 
+      const res = await fetch(`${process.env.NEXT_PUBLIC_BACKEND_LINK}/HandleOrderAccept?orderNumber=${value}`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ orderStatus: 'done' }),
+      });
 
+      if (!res.ok) throw new Error('Network response was not ok');
+      const text = await res.text();
+      console.log(text);
 
-                    const res = await fetch(`${process.env.NEXT_PUBLIC_BACKEND_LINK}/HandleOrderAccept?orderNumber=${value}`, {
-                        method: 'POST',
-                        headers: {
-                            'Content-Type': 'application/json',
-                        },
-                        body: JSON.stringify({ orderStatus: 'done' }),
-                    });
+      // Jump back to current-order page
+      if (text === "Successfully logged proposal change") router.push('/protected/current-orders');
 
-                    if (!res.ok) throw new Error('Network response was not ok');
-                    const text = await res.text();
-                    console.log(text);
+    } catch (error) {
+      console.error('Error during proposing change to manager log:', error);
+    }
+  };
 
-                    //jump back to current-order page
-                    if(text === "Successfully logged proposal change") router.push('/protected/current-orders');
-
-        } catch (error) {
-            console.error('Error during proposing change to manager log:', error);
-        }
-    };
-
-    return (
-        <Button onClick={handleClick} className="bg-red-600 text-white p-2">
-            Done
-        </Button>
-    );
+  return (
+    <Button onClick={handleClick} className="bg-red-600 text-white p-2">
+      Done
+    </Button>
+  );
 }
